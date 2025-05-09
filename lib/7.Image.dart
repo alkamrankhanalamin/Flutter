084d9e64f41f385b98e9ad419f5b72dbc0c72934
Kamran <alkamrankhanalamin@gmail.com>
2025-05-09 11:49:32 +0600

    After 1 - 6 Update

---
+++ lib/7.Image.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ImageApk extends StatelessWidget {
+  const ImageApk({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+      ),
+
+      body: Padding(padding: EdgeInsets.only(top: 15), child: Column(
+        children: [
+          Center(
+            child: Image.network('https://logos-world.net/wp-content/uploads/2024/08/Notion-Logo.png',
+              width: 100,
+              height: 100,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Image.asset('assets/palestine.png',
+          height:100, width: 100,),
+
+        ],
+      ),),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:ostad_flutter/3.container.dart';
 import 'package:ostad_flutter/4.floating_button.dart';
 import 'package:ostad_flutter/5.textfield.dart';
 import 'package:ostad_flutter/6.TextFormField.dart';
+import 'package:ostad_flutter/7.Image.dart';
 
-import '7.practice.dart';
+
 main() {
-runApp(Login_Form());
+runApp(ImageApk());
 }
 
 
